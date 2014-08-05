285aa56c3580e74f35ef6e1654c21e6f9db39fd3
Mike Pennisi <mike@mikepennisi.com>
2014-08-05 08:36:04 -0400

    Fix application Router unit tests
    The application Router expects the document body to contain a `<main>`
    element. Without it, all instances create a Layout without an element,
    resulting in an error when LayoutManager attempts to render.
    
    Ensure that the necessary element is present in the document before each
    Router unit test.

--- test/mocha/specs/boilerplate/router.spec.js
+++ test/mocha/specs/boilerplate/router.spec.js
@@ -6,6 +6,16 @@ define(function(require) {
 
   // Test that the Router exists.
   describe("Router", function() {
+
+    beforeEach(function() {
+      this.mainEl = document.createElement('main');
+      document.body.appendChild(this.mainEl);
+    });
+
+    afterEach(function() {
+      document.body.removeChild(this.mainEl);
+    });
+
     it("should exist", function() {
       expect(Router).to.exist;
       expect(new Router()).to.be.an.instanceof(Backbone.Router);
